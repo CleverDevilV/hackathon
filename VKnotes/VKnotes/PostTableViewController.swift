2b01d8f2195341623a4583a1a3921a3464a6528a
Viter <ViterDA@icloud.com>
2019-11-16 19:28:15 +0300

    Background и анимация кнопки входа

--- VKnotes/VKnotes/PostTableViewController.swift
+++ VKnotes/VKnotes/PostTableViewController.swift
@@ -9,123 +9,130 @@
 import UIKit
 
 class PostTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    var tableView = UITableView()
-    
-    var postsInCategories = PostModel.shared
-    
-//    var posts : [String] = []
-
-    var pick = 0
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        //    self.title = "Second"
-        self.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        postsInCategories.posts = [Post(text: "first", image: nil, isPicked: false)]
-        
-        self.view.backgroundColor = .white
-        
-        tableView.frame = self.view.frame
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-//        tableView.register(UITableViewCell.self.self, forCellReuseIdentifier: "posts")
-        tableView.register(PostsTableViewCell.self, forCellReuseIdentifier: PostsTableViewCell.reuseId)
-        
-        tableView.tableFooterView = UIView()
-        view.addSubview(tableView)
-        // Do any additional setup after loading the view.
-        
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPost))
-        let editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editList))
-        
-        navigationItem.rightBarButtonItems = [addButton, editButton]
-    }
-    
-    @objc
-    func addPost() {
-        
-        let addTaskAllert = UIAlertController(title: "Введите ссылку на новый пост", message: nil, preferredStyle: .alert)
-        
-        addTaskAllert.addTextField(configurationHandler: nil)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-        let okAction = UIAlertAction(title: "OK", style: .default){
-            _ in
-            
-            let textField = addTaskAllert.textFields![0] as UITextField
-            
-            guard let text = textField.text else {
-                 return
-            }
-            
-            let post = Post(text: textField.text!, image: nil, isPicked: false)
-            
-            self.postsInCategories.posts.append(post)
-//            self.posts.append(text)
-                                    
-            self.tableView.reloadData()
-        }
-        
-        addTaskAllert.addAction(cancelAction)
-        addTaskAllert.addAction(okAction)
-        present(addTaskAllert, animated: true, completion: nil)
-        tableView.reloadData()
-    }
-    
-    @objc
-    private func editList() {
-        tableView.setEditing(!tableView.isEditing, animated: true)
-        navigationItem.leftBarButtonItem?.title = tableView.isEditing ? "Done" : "Edit"
-    }
+	
+	var tableView = UITableView()
+	
+	var postsInCategories = PostModel.shared
+	
+	//    var posts : [String] = []
+	
+	var pick = 0
+	
+	init() {
+		super.init(nibName: nil, bundle: nil)
+		//    self.title = "Second"
+		self.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		postsInCategories.posts = [Post(text: "first", image: nil, isPicked: false)]
+		
+		self.view.backgroundColor = .white
+		
+		tableView.frame = self.view.frame
+		tableView.delegate = self
+		tableView.dataSource = self
+		
+		//        tableView.register(UITableViewCell.self.self, forCellReuseIdentifier: "posts")
+		tableView.register(PostsTableViewCell.self, forCellReuseIdentifier: PostsTableViewCell.reuseId)
+		
+		tableView.tableFooterView = UIView()
+		
+		let backImage = UIImageView(image: UIImage(named: "back_2"))
+		backImage.frame = self.tableView.frame
+		backImage.contentMode = .scaleAspectFill
+		self.tableView.backgroundView = backImage
+		
+		view.addSubview(tableView)
+		// Do any additional setup after loading the view.
+		
+		let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPost))
+		let editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editList))
+		
+		navigationItem.rightBarButtonItems = [addButton, editButton]
+	}
+	
+	@objc
+	func addPost() {
+		
+		let addTaskAllert = UIAlertController(title: "Введите ссылку на новый пост", message: nil, preferredStyle: .alert)
+		
+		addTaskAllert.addTextField(configurationHandler: nil)
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		
+		let okAction = UIAlertAction(title: "OK", style: .default){
+			_ in
+			
+			let textField = addTaskAllert.textFields![0] as UITextField
+			
+			guard let text = textField.text else {
+				return
+			}
+			
+			let post = Post(text: textField.text!, image: nil, isPicked: false)
+			
+			self.postsInCategories.posts.append(post)
+			//            self.posts.append(text)
+			
+			self.tableView.reloadData()
+		}
+		
+		addTaskAllert.addAction(cancelAction)
+		addTaskAllert.addAction(okAction)
+		present(addTaskAllert, animated: true, completion: nil)
+		tableView.reloadData()
+	}
+	
+	@objc
+	private func editList() {
+		tableView.setEditing(!tableView.isEditing, animated: true)
+		navigationItem.leftBarButtonItem?.title = tableView.isEditing ? "Done" : "Edit"
+	}
 }
 
 //MARK: - protocols
 extension PostTableViewController {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postsInCategories.posts.count//posts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PostsTableViewCell.reuseId, for: indexPath) as! PostsTableViewCell
-        
-        let post = postsInCategories.posts[indexPath.row]
-        
-        cell.noteLabel.text = post.text //posts[indexPath.row]
-        print("cell indexPath: ", indexPath)
-        cell.noteImage.isHidden = !post.isPicked
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return postsInCategories.posts.count//posts.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: PostsTableViewCell.reuseId, for: indexPath) as! PostsTableViewCell
+		
+		let post = postsInCategories.posts[indexPath.row]
+		
+		cell.noteLabel.text = post.text //posts[indexPath.row]
+		print("cell indexPath: ", indexPath)
+		cell.noteImage.isHidden = !post.isPicked
 		
 		print("---------------")
 		print("posts", postsInCategories.posts)
 		cell.accessoryType = .disclosureIndicator
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete {
+		cell.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+		
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		
+		if editingStyle == .delete {
 			postsInCategories.posts.remove(at: indexPath.row)
-            
-            tableView.reloadData()
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return UITableViewCell.EditingStyle.delete
-    }
+			
+			tableView.reloadData()
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+		return UITableViewCell.EditingStyle.delete
+	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		let cell = tableView.cellForRow(at: indexPath)
@@ -136,8 +143,8 @@ extension PostTableViewController {
 		
 		navigationController?.pushViewController(postsVC, animated: true)
 	}
-    
-   func tableView(_ tableView: UITableView,
+	
+	func tableView(_ tableView: UITableView,
 				   leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
 	{
 		let tickAction = UIContextualAction(style: .normal, title:  "Close", handler: { (ac: UIContextualAction, view: UIView, success: (Bool) -> Void) in
--- VKnotes/VKnotes/StartViewController.swift
+++ VKnotes/VKnotes/StartViewController.swift
@@ -9,46 +9,53 @@
 import UIKit
 
 class StartViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.white
-        
-        helloLabel.center = CGPoint(x: view.center.x, y: view.center.y - 50)
-        view.addSubview(helloLabel)
-        
-        startButton.center = CGPoint(x: view.center.x, y: view.center.y + 50)
-        view.addSubview(startButton)
-    }
-    
-    var helloLabel: UILabel = {
-        let helloLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        helloLabel.text = "Добро пожаловать!"
-        helloLabel.font = UIFont(name: "AmericanTypewriter-Bold", size: 25)
-        helloLabel.numberOfLines = 2
-        helloLabel.textAlignment = .center
-        return helloLabel
-    }()
-       
-    var startButton: UIButton = {
-        let startButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
-        startButton.backgroundColor = .systemBlue
-        startButton.setTitle("Начать", for: .normal)
-        startButton.layer.cornerRadius = 15
-        startButton.addTarget( self, action: #selector(start), for: .touchUpInside)
-        return startButton
-    }()
-       
-    @objc
-    func start(){
-        if AppDelegate.defaults.bool(forKey: "loggedIn"){
-            AppDelegate.shared.rootViewController.switchToMainScreen()
-        } else {
-            AppDelegate.shared.rootViewController.switchToLogout()
-        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.setNavigationBarHidden(true, animated: true)
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = UIColor.white
+		
+		helloLabel.center = CGPoint(x: view.center.x, y: view.center.y - 50)
+		view.addSubview(helloLabel)
+		
+		startButton.center = CGPoint(x: view.center.x, y: view.center.y + 50)
+		view.addSubview(startButton)
+	}
+	
+	var helloLabel: UILabel = {
+		let helloLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+		helloLabel.text = "Добро пожаловать!"
+		helloLabel.font = UIFont(name: "AmericanTypewriter-Bold", size: 25)
+		helloLabel.numberOfLines = 2
+		helloLabel.textAlignment = .center
+		return helloLabel
+	}()
+	
+	var startButton: UIButton = {
+		let startButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+		startButton.backgroundColor = .systemBlue
+		startButton.setTitle("Начать", for: .normal)
+		startButton.layer.cornerRadius = 15
+		startButton.addTarget( self, action: #selector(start(_:)), for: .touchUpInside)
+		return startButton
+	}()
+	
+	@objc
+	func start(_ sender: UIButton){
+		
+		UIView.animate(withDuration: 2, delay: 0, options: [], animations: {
+			self.startButton.layer.opacity = 0
+			self.helloLabel.layer.opacity = 0
+		}, completion: {
+			_ in
+			if AppDelegate.defaults.bool(forKey: "loggedIn"){
+				AppDelegate.shared.rootViewController.switchToMainScreen()
+			} else {
+				AppDelegate.shared.rootViewController.switchToLogout()
+			}
+		})
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		navigationController?.setNavigationBarHidden(true, animated: true)
+	}
 }
